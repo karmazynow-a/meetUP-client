3ea7dc2e097e68583f0f563d2421dc570650d1b1
Aleksandra Poreba <aleks.poreba97@gmail.com>
2020-05-03 11:08:24 +0200

    Add author_id to event/person

--- meetUP-server/src/model/event/EventDaoImpl.java
+++ meetUP-server/src/model/event/EventDaoImpl.java
@@ -100,7 +100,7 @@ public class EventDaoImpl implements EventDao{
 	
 	// get all events that one is participant
 	public List<Map<String, Object>> getEventByPersonID(int id) {
-		String query = "SELECT e.id, e.name, e.date, p.lname AS author_lname, p.fname AS author_fname FROM participation r "
+		String query = "SELECT e.id, e.name, e.date, e.author_id, p.lname AS author_lname, p.fname AS author_fname FROM participation r "
 				+ "JOIN event e ON e.id=r.event_id JOIN person p ON p.id=e.author_id "
 				+ "WHERE r.person_id=?";
 		
